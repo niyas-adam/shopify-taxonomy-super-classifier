9f1eb0d8e315ebcdb018a747da861431269bc436
niyas-adam <niyasadam19@gmail.com>
2026-08-27 13:04:38 +0530

    Add direct stats_view endpoint for /api/stats/

--- classification/urls.py
+++ classification/urls.py
@@ -1,12 +1,18 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .api import ProductViewSet, TaxonomyCategoryViewSet, ClassificationViewSet
+from .api import (
+    TaxonomyCategoryViewSet, ProductViewSet,
+    ClassificationViewSet, ClassificationBatchViewSet, StatsViewSet,
+    stats_view
+)
 
 router = DefaultRouter()
-router.register(r'products', ProductViewSet)
 router.register(r'taxonomy', TaxonomyCategoryViewSet)
+router.register(r'products', ProductViewSet)
 router.register(r'classifications', ClassificationViewSet)
+router.register(r'batches', ClassificationBatchViewSet)
 
 urlpatterns = [
+    path('stats/', stats_view, name='stats'),
     path('', include(router.urls)),
 ]
